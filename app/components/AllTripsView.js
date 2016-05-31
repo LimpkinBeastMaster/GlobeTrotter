ddd6e5530934ee5c902e16c95c162cd47bf4a35a
Asif Ahmed <asifuzzaman.ahmed@gmail.com>
2016-05-31 15:41:14 -0700

    Modularize AllTripsView

--- app/components/AllTripsView.js
+++ app/components/AllTripsView.js
@@ -1,59 +1,15 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
-class Home extends React.Component {
+class AllTripsView extends React.Component {
   render() {
-  	var searchButton = {
-  		height: "50px",
-  		width: "150px",
-  		marginRight: "20px",
-  		float: "left"
-  	}
-
-  	var searchBox = {
-  		width: "1000px",
-  		marginTop: "200px",
-  		marginLeft: "auto",
-  		marginRight: "auto",
-  	}
-
-  	var searchContainer = {
-  		width: "800px",
-	    boxSizing: "border-box",
-	    border: "2px solid #ccc",
-	    borderRadius: "4px",
-	    fontSize: "16px",
-	    backgroundColor: "white",
-	    backgroundImage: "url('/img/searchicon.png')",
-	    backgroundSize: "27px",
-	    backgroundPosition: "10px 10px", 
-	    backgroundRepeat: "no-repeat",
-	    padding: "12px 20px 12px 40px",
-	    webkitTransition: "width 0.4s ease-in-out",
-	    transition: "width 0.4s ease-in-out",
-	    float: "left"
-  	}
 
     return (
     	<div className='all-trips-view'>
-	      <div className='search-box' style={searchBox}>
-	      	<button 
-	      		className='btn btn-primary dropdown-toggle' 
-	      		type="button" 
-	      		data-toggle="dropdown" 
-	      		style={searchButton}>
-	      		Location
-	      		<span class="carat">
-	      		</span>
-	      	</button>
-	        <input 
-	        	style={searchContainer} 
-	        	type="text" 
-	        	name="location-search" 
-	        	placeholder="Find your next trip.." />
-	      </div>
+    		<SearchBar />
       </div>
     );
   }
 }
 
-export default Home;
+export default AllTripsView;
