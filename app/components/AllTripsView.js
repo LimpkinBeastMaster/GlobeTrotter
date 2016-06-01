053ab78099f6bd1c3e0d9e2f490c41d63d602743
Asif Ahmed <asifuzzaman.ahmed@gmail.com>
2016-05-31 18:22:31 -0700

    Modulate Trips'

--- app/components/AllTripsView.js
+++ app/components/AllTripsView.js
@@ -5,22 +5,31 @@ import TripList from './TripList';
 class AllTripsView extends React.Component {
   render() {
 
+  	var search = {
+  		height: "200px",
+  		width: "1000px",
+  		marginLeft: "auto",
+  		marginRight: "auto"
+  	}
+
   	var trips = {
   		width: "1000px",
   		clear: "both",
   		marginLeft: "auto",
   		marginRight: "auto"
-  	}
+  		}
 
     return (
-    	<div className='all-trips-view'>
+    	<div className='all-trips-view' style={search}>
     		<div className="search-bar">
     			<SearchBar />
     		</div>
     		<div className="trips" style={trips}>
-    			<TripList />
-    			<TripList />
-    			<TripList />
+    			<TripList likes="829"  start="Tokyo" end="Koyoto" />
+    			<TripList likes="429" start="Tokyo" end="Shizuoka" />
+    			<TripList likes="225" start="Tokyo" end="Akita" />
+    			<TripList likes="211" start="Tokyo" end="Matsumoto" />
+    			<TripList likes="120" start="Tokyo" end="Fukoka" />
     		</div>
       </div>
     );
--- app/components/TripList.js
+++ app/components/TripList.js
@@ -1,31 +1,52 @@
 import React from 'react';
 
-class TripList extends React.Component {
-	render() {
-
-		var tripDisplay = {
-  		height: "50px",
-  		width: "950px",
-  		marginLeft: "auto",
-  		marginRight: "auto",
-  		marginTop: "15px",
-  		marginBottom: "10px"
-  	}
-
-		return (
-			<div className="trip-list">
-				<button 
-		  		className='btn btn-primary' 
-		  		type="button" 
-		  		data-toggle="dropdown" 
-		  		style={tripDisplay}>
-		  		Tokyo To Kyoto
-		  		<span class="carat">
-		  		</span>
-		  	</button>
-			</div>
-		);
-	}
+var tripDisplay = {
+	fontSize: "22px", 
+	height: "60px",
+	width: "900px",
+	marginLeft: "auto",
+	marginRight: "auto",
+	marginTop: "10px",
+	marginBottom: "10px"
 }
+
+var likes = {
+	height: "50px",
+	width: "65px",
+	padding: "5px",
+	marginTop: "5px",
+	marginBottom: "5px",
+	marginRight: "10px",
+	borderStyle: "solid",	
+	float: "left"
+}
+
+var tripBar = {
+	fontSize: "18px", 
+	height: "50px",
+	width: "820px",
+	marginTop: "5px",
+	marginBottom: "5px",
+	float: "left"
+}
+
+var TripList = (props) => (
+
+	<div className="trip-list" style={tripDisplay}>
+		<div className="likes" style={likes}>
+			+{props.likes}
+		</div>
+		<button 
+  		className='btn btn-primary' 
+  		type="button" 
+  		data-toggle="dropdown" 
+  		style={tripBar}>
+  		From {props.start} To {props.end}
+  		<span class="carat">
+  		</span>
+  	</button>
+	</div>
+);
+
 
 export default TripList;
