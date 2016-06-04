64b65f59a4a368aa786131cc1a3ab8d3d65ebf3c
Ben Roa <benihanas1@gmail.com>
2016-06-04 16:44:27 -0700

    All combined files from the merge + fixed the navbar

--- app/components/App.js
+++ app/components/App.js
@@ -13,39 +13,60 @@ import Globe from './Globe'
 // <div style={map}>
 //   <Maps>as</Maps>
 // </div>
+import { Router, Route, Link  } from 'react-router'
+import { Navbar, NavItem, Nav, FormGroup, FormControl, Button } from 'react-bootstrap'
+import { LinkContainer, IndexRoute, IndexLinkContainer } from 'react-router-bootstrap'
 
-
+function fxn(path){
+  this.props.history.push(path);
+}
 
 class App extends React.Component {
   render() {
     return (
-
       <div>
-      	<span>
-          <NavLink 
-            to="/login">
-            Login
-          </NavLink>
-          <strong> | </strong>
-          <NavLink 
-            to="/createtrips">
-            Create Trips
-          </NavLink>
-          <strong> | </strong>
-          <NavLink 
-            to="/mytrips">
-            My Trips
-          </NavLink>
-          <strong> | </strong>
-          <NavLink 
-            to="/alltrips">
-            AllTrips
-          </NavLink>        
-        </span>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+             <a href="#">TravelApp</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to="/" activeClassName="active" onClick={fxn.bind(this, '/')}>
+                <NavItem eventKey={1}>Home</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login" activeClassName="active" onClick={fxn.bind(this, '/login')}>
+                <NavItem eventKey={2}>Login</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login" activeClassName="active" onClick={fxn.bind(this, '/login')}>
+                <NavItem eventKey={3}>Sign Up</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/createtrips" activeClassName="active" onClick={fxn.bind(this, '/createtrips')}>
+                <NavItem eventKey={4}>Build Trip</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/alltrips" activeClassName="active" onClick={fxn.bind(this, '/alltrips')}>
+                <NavItem eventKey={5}>Trips Feed</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/mytrips" activeClassName="active" onClick={fxn.bind(this, '/mytrips')}>
+                <NavItem eventKey={6}>My Trips</NavItem>
+              </LinkContainer>
+            </Nav>
+            <Nav pullRight>
+              <Navbar.Form>
+                <FormGroup>
+                  <FormControl type="text" placeholder="Find your next trip..." />
+                </FormGroup>
+              </Navbar.Form>
+            </Nav>
+          </Navbar.Collapse>        
+        </Navbar>
         {this.props.children}
       </div>
     );
   }
 }
+
 
 export default App;
