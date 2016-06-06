4eb5d4dcaecf5bf22fcddbc2bfc0e60bcf076bfb
Albert Han <hanalbert87@gmail.com>
2016-06-06 16:24:58 -0700

    redirect for trip Details

--- app/components/AllTripsView.js
+++ app/components/AllTripsView.js
@@ -3,86 +3,84 @@ import SearchBar from './SearchBar';
 import TripList from './TripList';
 import AllTripsStore from '../stores/AllTripsStore';
 import AllTripsActions from '../actions/AllTripActions';
-import allData from '../stores/data/allData'
-import { Row, PageHeader } from 'react-bootstrap'
+
 import { search, trips } from '../stylesheets/style';
 import { searchButton, searchBox, searchContainer } from '../stylesheets/style';
 
 class AllTripsView extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = AllTripsStore.getState();
-    this.onChange = this.onChange.bind(this);
-  }
-
-  componentDidMount() {
-    AllTripsStore.listen(this.onChange);
-    AllTripsActions.GetTrips();
-  }
-
-  componentWillUnmount() {
-    AllTripsStore.unlisten(this.onChange);
-  }
-
-  onChange(state) {
-    this.setState(state);
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    let searchQuery = this.state.searchQuery.trim();
-
-    if (searchQuery) {
-      var queryResult = [];
-      this.state.trips.forEach(function(trip) {
-        if(trip.title.toLowerCase().indexOf(searchQuery.toLowerCase()) != -1) {
-          queryResult.push(trip);
-        }
-      });      
-      console.log('QUERYRESULT', queryResult);
-              
-      this.setState({
-        trips : queryResult
-      })
+    constructor(props) {
+        super(props);
+        this.state = AllTripsStore.getState();
+        this.onChange = this.onChange.bind(this);
     }
-  }
 
-  handleTripClick(trip, index, type, e) {
-      e.preventDefault();
-      console.log('name, title', trip, index, type);
-      
-      if(type !== 3) {
-          AllTripsActions.ChangeLikes(trip, index, type);
-      }
-  }
+    componentDidMount() {
+        AllTripsStore.listen(this.onChange);
+        AllTripsActions.GetTrips();
+    }
 
-  render() {
-    var tripArr = this.state.trips.map((trip, indx) => {
-      return (<TripList key={indx} index={indx} trip={trip} clickfxn={this.handleTripClick.bind(this)}/>)
-    });
+    componentWillUnmount() {
+        AllTripsStore.unlisten(this.onChange);
+    }
 
-    return (
-      <div>
-      	<div className='Row'>
-          <div className="col-md-2">
-          </div>
-          <div className="col-md-8">
-            <PageHeader>Trips Feed <small>Searching Japan</small></PageHeader>
-          </div>
-          <div className="col-md-2">
-          </div>  
-      		<div style={{marginBottom: "10px", clear: "both"}}>
+    onChange(state) {
+        this.setState(state);
+    }
+
+    handleSubmit(event) {
+
+        event.preventDefault();
+
+        let searchQuery = this.state.searchQuery.trim();
+
+        if (searchQuery) {
+
+            var queryResult = [];
+            this.state.trips.forEach(function(trip) {
+                if(trip.title.toLowerCase().indexOf(searchQuery.toLowerCase()) != -1) {
+                    queryResult.push(trip);
+                }
+            });
+              
+            console.log('QUERYRESULT', queryResult);
+                
+            this.setState({
+                trips : queryResult
+            })
+        }
+    }
+
+    handleTripClick(trip, index, type, e) {
+        e.preventDefault();
+        console.log('name, title', trip, index, type);
+        
+        if(type !== 3) {
+            AllTripsActions.ChangeLikes(trip, index, type);
+        }
+    }
+
+    render() {
+        var tripArr = this.state.trips.map((trip, indx) => {
+            return (<TripList key={indx} index={indx} trip={trip} history={this.props.history} clickfxn={this.handleTripClick.bind(this)}/>)
+        });
+
+        return (
+          <div className='all-trips-view' style={search}>
             <div className="search-bar">
-              <SearchBar />
+              <form ref='searchForm' className='navbar-form navbar-left animated' onSubmit={this.handleSubmit.bind(this)}>
+                    <div className='input-group'>
+                        <input style={searchContainer} type='text' className='form-control' placeholder={'Find your next trip...'} value={this.state.searchQuery} onChange={AllTripsActions.updateSearchQuery} />
+                        <span className='input-group-btn'>
+                        <button className='btn btn-default' onChange={this.handleSubmit.bind(this)}><span className='glyphicon glyphicon-search'></span></button>
+                        </span>
+                    </div>
+                    </form>
             </div>
-      		</div>
-      		<div style={{clear: "both"}}>
-      		  {tripArr}
-      		</div>
-        </div>
-      </div>
+            <div className="trips" style={trips}>
+              {tripArr}
+            </div>
+            </div>
     );
   }
 }
--- app/components/TripList.js
+++ app/components/TripList.js
@@ -1,52 +1,68 @@
-import React from 'react'
-import { Router, Route, Link } from 'react-router'
-import { Row, Button, Well, ButtonGroup } from 'react-bootstrap'
-import { tripDisplay, likes, tripBar } from '../stylesheets/style'
-import CreateTripActions from '../actions/CreateTripActions'
+import React from 'react';
+import { tripDisplay, likes, tripBar } from '../stylesheets/style';
+import CreateTripActions from '../actions/CreateTripActions';
+import UserActions from '../actions/UserActions'
+import UserStore from '../stores/UserStore'
+import AllTripActions from '../actions/AllTripActions'
+import CreateTripStore from '../stores/CreateTripStore'
+import AllTripsStore from '../stores/AllTripsStore'
 
 class TripList extends React.Component {
 
   constructor(props) {
-    super(props);    
+    super(props);
+    this.state = UserStore.getState();
+    this.onChange = this.onChange.bind(this);
   }
 
-  clickHandler(e) {
+  componentDidMount() {
+    CreateTripStore.listen(this.onChange);
+  }
+
+  componentWillUnmount() {
+    CreateTripStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    event.preventDefault();
+    console.log('props.history:', this.props.history);
+    console.log('this.props:', this.props)
+    const path = '/createtrips';
+    this.props.history.push(path);
+  }
+
+  clickHandler(event) {
     // console.log(e.target.id)
     // console.log(e.currentTarget.id)
-    CreateTripActions.GetTrip(e.currentTarget.id)
+    CreateTripActions.GetTrip(event.currentTarget.id);
   }
 
   render () {
     var sign = '';
     this.props.trip.likes > 0 ? sign = '+' : sign = ''; 
-  	return (
-      <div className="Row" style={{clear: "both"}}>
-        <div className="col-md-2">
-          <Button type="button" bsSize="large" disabled block>
-            {sign + this.props.trip.likes}
-          </Button>
-        </div>
-          <div className="col-md-8" style={{ margin: '0 auto 10px' }}>
-          <Button type="button" bsSize="large" bsStyle="primary" block
-            id={this.props.trip.id}
-            onClick = {this.clickHandler.bind(this)}>
-            {this.props.trip.title}
-          </Button> 
-        </div> 
-        <div className="col-md-2">
-          <ButtonGroup>
-            <Button bsStyle="info" bsSize="large" onClick={this.props.clickfxn.bind(null, this.props.trip, this.props.index, 1)}>
-              <span className="glyphicon glyphicon-arrow-up"></span>
-            </Button>
-            <Button bsStyle="warning" bsSize="large" onClick={this.props.clickfxn.bind(null, this.props.trip, this.props.index, 2)}>
-              <span className="glyphicon glyphicon-arrow-down"></span>
-            </Button>
-            <Button bsStyle="danger" bsSize="large" onClick={this.props.clickfxn.bind(null, this.props.trip, this.props.index, 3)}>
-              <span className="glyphicon glyphicon-remove-sign"></span>
-            </Button>
-          </ButtonGroup>
-        </div> 
-    	</div>
+    return (
+      <div className="trip-list" style={tripDisplay}>
+
+        <span className="likes" style={likes}>
+          {sign+JSON.stringify(this.props.trip.likes).replace(/["]/g, '')}
+        </span>
+        <span className='pull-right'>
+        <button 
+          id={this.props.trip.id}
+          className="btn btn-primary" 
+          type="button" 
+          data-toggle="dropdown" 
+          style={tripBar}
+          onClick = {this.clickHandler.bind(this)}>
+          {this.props.trip.title} 
+          <span className="carat">
+          </span>
+        </button> 
+        <a href="#" className="btn btn-default" onClick={this.props.clickfxn.bind(null, this.props.trip, this.props.index, 1)}><span className="glyphicon glyphicon-arrow-up"></span></a>
+        <a href="#" className="btn btn-default" onClick={this.props.clickfxn.bind(null, this.props.trip, this.props.index, 2)}><span className="glyphicon glyphicon-arrow-down"></span></a>
+        <a href="#" className="btn btn-default" onClick={this.props.clickfxn.bind(null, this.props.trip, this.props.index, 3)}><span className="glyphicon glyphicon-remove-sign"></span></a>
+       </span>
+      </div>
     )
   };
 };
